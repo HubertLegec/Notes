85f7df88ed0bdb01e35bb6ddebb0d39dd396bc84
HubertLegec <hubert.legec@gmail.com>
2017-01-22 15:48:54 +0100

    init

---
+++ Notes/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+interface LayoutProps {
+
+}
+
+interface LayoutState {
+
+}
+
+export default class Layout extends React.Component<LayoutProps, LayoutState> {
+    render() {
+        return <div>
+            hello
+        </div>
+    }
+}
